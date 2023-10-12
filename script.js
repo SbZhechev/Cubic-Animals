b13c09d9417de397331ce8649cbca7958042de39
sbzhechev <sbzhechev@gmail.com>
2023-10-12 16:50:04 +0300

    Added stork to the scene.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import StorkMesh from './animals/stork';
 
 // Sizes
 const sizes = {
@@ -8,10 +10,11 @@ const sizes = {
 
 // Scene
 const scene = new THREE.Scene();
+scene.background = new THREE.Color('lightskyblue');
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.z = 5;
+camera.position.z = 6;
 camera.position.y = 2;
 scene.add(camera);
 
@@ -20,6 +23,10 @@ const canvas = document.getElementById('webgl');
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Orbit Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 // Resize
 window.addEventListener('resize', () => {
@@ -36,16 +43,25 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-// Cube
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-camera.lookAt(cube.position);
+// Ground
+const geometry = new THREE.BoxGeometry( 3, 0.2, 3 );
+const material = new THREE.MeshBasicMaterial( { color: 'limegreen' } );
+const ground = new THREE.Mesh( geometry, material );
+scene.add( ground );
+
+// Add animals
+StorkMesh.position.y = 1.2;
+scene.add(StorkMesh);
+
+// Clock
+const clock = new THREE.Clock();
 
 // Render scene
 function animate() {
-  cube.rotation.y += 0.005;
+  const elapsedTime = clock.getElapsedTime();
+
+  StorkMesh.position.y += Math.sin(elapsedTime) * 0.01;
+
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
